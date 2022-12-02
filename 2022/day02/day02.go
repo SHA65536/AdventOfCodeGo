16095978f62a43dc63dd377878dcd5e17096ac21
SHA65536 <53488697+SHA65536@users.noreply.github.com>
2022-12-02 13:36:30 +0200

    added consts for readability

--- 2022/day02/day02.go
+++ 2022/day02/day02.go
@@ -6,6 +6,12 @@ import (
 
 // https://adventofcode.com/2022/day/2
 
+const ( // Score for result
+	Lose = 0
+	Draw = 3
+	Win  = 6
+)
+
 func RockPaperScissors(guide string) (string, error) {
 	var score int
 	// Looping over the guide
@@ -14,11 +20,11 @@ func RockPaperScissors(guide string) (string, error) {
 		him := int(guide[i] - 'A')   // Their pick
 		score += you + 1
 		if you == him { // Same shape draws
-			score += 3
+			score += Draw
 		} else if (you+1)%3 == him { // Shape below loses (looping)
-			score += 0
+			score += Lose
 		} else if (you+2)%3 == him { // Shape above wins (looping)
-			score += 6
+			score += Win
 		}
 	}
 	return strconv.Itoa(score), nil
@@ -32,11 +38,13 @@ func RockPaperScissorsModified(guide string) (string, error) {
 		res := int(guide[i+2] - 'X') // The result
 		if res == 0 {                // Lose
 			score += ((him + 2) % 3) + 1 // Shape below loses (looping)
+			score += Lose
 		} else if res == 1 { // Draw
-			score += him + 1 + 3 // Same shape draws
+			score += him + 1 // Same shape draws
+			score += Draw
 		} else if res == 2 { // Win
 			score += ((him + 1) % 3) + 1 // Shape above wins (looping)
-			score += 6
+			score += Win
 		}
 	}
 	return strconv.Itoa(score), nil
