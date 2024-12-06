232ca0613f3518fbd41e19ab4e084f90833a9030
SHA65536 <53488697+SHA65536@users.noreply.github.com>
2024-12-06 14:22:57 +0200

    performance improvement on day 6 2024

--- 2024/day06/day06.go
+++ 2024/day06/day06.go
@@ -19,18 +19,23 @@ func Star1(input *helper.InputReader) (string, error) {
 	var pos, dir = findGuard(board)
 
 	for inside(board, pos) {
+		// Mark unique visited spots and add to result
 		if board[pos[0]][pos[1]] != 'X' {
 			board[pos[0]][pos[1]] = 'X'
 			res++
 		}
 
+		// Next position
 		npos := [2]int{pos[0] + dirs[dir][0], pos[1] + dirs[dir][1]}
+		// If next position is blocked
 		for inside(board, npos) && board[npos[0]][npos[1]] == '#' {
+			// Rotate until you are not blocked
 			dir = (dir + 1) % 4
 			npos = [2]int{pos[0] + dirs[dir][0], pos[1] + dirs[dir][1]}
 		}
 
-		pos = [2]int{pos[0] + dirs[dir][0], pos[1] + dirs[dir][1]}
+		// Move to the next position
+		pos = npos
 	}
 
 	return strconv.Itoa(res), nil
@@ -44,6 +49,7 @@ func makeBoard(input *helper.InputReader) [][]byte {
 	return res
 }
 
+// Find guard position and direction
 func findGuard(in [][]byte) ([2]int, int) {
 	for i := 0; i < len(in); i++ {
 		for j := 0; j < len(in[0]); j++ {
@@ -71,15 +77,31 @@ func Star2(input *helper.InputReader) (string, error) {
 	var res int
 
 	var board = makeBoard(input)
+	var initialPos, initialDir = findGuard(board)
+	var pos, dir = initialPos, initialDir
 
+	// Mark all spots where we an obstacle will change the path
+	for inside(board, pos) {
+		board[pos[0]][pos[1]] = 'X'
+
+		npos := [2]int{pos[0] + dirs[dir][0], pos[1] + dirs[dir][1]}
+		for inside(board, npos) && board[npos[0]][npos[1]] == '#' {
+			dir = (dir + 1) % 4
+			npos = [2]int{pos[0] + dirs[dir][0], pos[1] + dirs[dir][1]}
+		}
+
+		pos = npos
+	}
+
+	// Check all possible places to place an obstacle
 	for i := range board {
 		for j := range board[i] {
-			if board[i][j] != '#' && board[i][j] != '^' {
+			if board[i][j] == 'X' && !(i == initialPos[0] && j == initialPos[1]) {
 				board[i][j] = '#'
-				if FindLoop(board) {
+				if FindLoop(board, initialPos, initialDir) {
 					res++
 				}
-				board[i][j] = '.'
+				board[i][j] = 'X'
 			}
 		}
 	}
@@ -87,17 +109,20 @@ func Star2(input *helper.InputReader) (string, error) {
 	return strconv.Itoa(res), nil
 }
 
-func FindLoop(board [][]byte) bool {
+func FindLoop(board [][]byte, pos [2]int, dir int) bool {
+	// dirboard[x][y] stores which directions we were when we walked in this spot
 	var dirBoard = make([][][4]bool, len(board))
 	for i := range dirBoard {
 		dirBoard[i] = make([][4]bool, len(board[0]))
 	}
-	var pos, dir = findGuard(board)
 
+	// Walk until you find a loop
 	for inside(board, pos) {
+		// If we were in the same position in the same direction, loop
 		if dirBoard[pos[0]][pos[1]][dir] {
 			return true
 		}
+		// Record current direction
 		dirBoard[pos[0]][pos[1]][dir] = true
 
 		npos := [2]int{pos[0] + dirs[dir][0], pos[1] + dirs[dir][1]}
@@ -106,7 +131,7 @@ func FindLoop(board [][]byte) bool {
 			npos = [2]int{pos[0] + dirs[dir][0], pos[1] + dirs[dir][1]}
 		}
 
-		pos = [2]int{pos[0] + dirs[dir][0], pos[1] + dirs[dir][1]}
+		pos = npos
 	}
 
 	return false
