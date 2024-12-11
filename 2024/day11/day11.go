8c147aa86d7d83e0bd38bc7b7aad7766eb5583f3
SHA65536 <53488697+SHA65536@users.noreply.github.com>
2024-12-11 07:40:29 +0200

    add day 11 2024

---
+++ 2024/day11/day11.go
@@ -0,0 +1,96 @@
+package day11
+
+import (
+	"adventofcode/helper"
+	"container/list"
+	"strconv"
+	"strings"
+)
+
+func Star1(input *helper.InputReader) (string, error) {
+	var res int
+
+	var stones = makeStones(input)
+
+	for i := 25; i > 0; i-- {
+		for e := stones.Front(); e != nil; e = e.Next() {
+			if e.Value.(int) == 0 {
+				e.Value = 1
+				continue
+			}
+			if left, right, ok := splitInteger(e.Value.(int)); ok {
+				e.Value = left
+				stones.InsertAfter(right, e)
+				e = e.Next()
+				continue
+			}
+			e.Value = e.Value.(int) * 2024
+		}
+	}
+
+	res = stones.Len()
+
+	return strconv.Itoa(res), nil
+}
+
+func makeStones(input *helper.InputReader) *list.List {
+	var res = list.New()
+
+	var _line, _ = input.ReadAll()
+	for _, num := range strings.Fields(_line) {
+		res.PushBack(helper.MustConvNum(num))
+	}
+
+	return res
+}
+
+func splitInteger(n int) (int, int, bool) {
+	nStr := strconv.Itoa(n)
+
+	if len(nStr)%2 != 0 {
+		return 0, 0, false
+	}
+
+	mid := (len(nStr)) / 2
+
+	return helper.MustConvNum(nStr[:mid]), helper.MustConvNum(nStr[mid:]), true
+}
+
+func Star2(input *helper.InputReader) (string, error) {
+	var res int
+
+	var cache = make(map[[2]int]int, 0)
+
+	var calcStone func(st int, it int) int
+	calcStone = func(st, it int) int {
+		if v, ok := cache[[2]int{st, it}]; ok {
+			return v
+		}
+		if it == 0 {
+			return 1
+		}
+
+		if st == 0 {
+			ret := calcStone(1, it-1)
+			cache[[2]int{st, it}] = ret
+			return ret
+		}
+
+		if l, r, ok := splitInteger(st); ok {
+			ret := calcStone(l, it-1) + calcStone(r, it-1)
+			cache[[2]int{st, it}] = ret
+			return ret
+		}
+
+		ret := calcStone(st*2024, it-1)
+		cache[[2]int{st, it}] = ret
+		return ret
+	}
+
+	var _line, _ = input.ReadAll()
+	for _, num := range strings.Fields(_line) {
+		res += (calcStone(helper.MustConvNum(num), 75))
+	}
+
+	return strconv.Itoa(res), nil
+}
---
+++ 2024/day11/day11_test.go
@@ -0,0 +1,26 @@
+package day11
+
+import (
+	"adventofcode/helper"
+	"embed"
+	"testing"
+)
+
+//go:embed *.txt
+var embed_fs embed.FS
+
+func TestStar1(t *testing.T) {
+	helper.TestStar(t, Star1, embed_fs, "55312", "190865")
+}
+
+func BenchmarkStar1(b *testing.B) {
+	helper.BenchmarkStar(b, Star1, embed_fs)
+}
+
+func TestStar2(t *testing.T) {
+	helper.TestStar(t, Star2, embed_fs, "65601038650482", "1609")
+}
+
+func BenchmarkStar2(b *testing.B) {
+	helper.BenchmarkStar(b, Star2, embed_fs)
+}
